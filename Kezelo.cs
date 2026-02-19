7a2697e65eb4741d8710693c4ed08ec0eafa3b94
user <user@petrik.hu>
2026-02-19 14:41:50 +0100

    Kezelo osztaly kesz

--- Kezelo.cs
+++ Kezelo.cs
@@ -8,5 +8,25 @@ namespace UveghazProjekt
 {
 	internal class Kezelo
 	{
+		private string nev;
+		private string azonosito;
+		private Szerepkor szerepkor;
+
+		public Kezelo(string nev, string azonosito, Szerepkor szerepkor)
+		{
+			this.nev = nev;
+			this.azonosito = azonosito;
+			this.szerepkor = szerepkor;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public string Azonosito { get => azonosito; set => azonosito = value; }
+		internal Szerepkor Szerepkor { get => szerepkor; set => szerepkor = value; }
+
+
+		public override string ToString()
+		{
+			return $"{this.nev}: {this.szerepkor}";
+		}
 	}
 }
