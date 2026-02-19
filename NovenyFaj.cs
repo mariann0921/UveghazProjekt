530536f42402734e34ee87fa84f3e6ab92f5d123
user <user@petrik.hu>
2026-02-19 14:51:53 +0100

    NovenyFaj osztály kész

--- NovenyFaj.cs
+++ NovenyFaj.cs
@@ -8,5 +8,32 @@ namespace UveghazProjekt
 {
 	internal class NovenyFaj
 	{
+		//o	Azonosito, Nev o IdealTalajnedvesseg, IdealHomersekleto MinSuruseg, OptimalisSuruseg, MaxSuruseg
+		
+		string nev;
+		int vizigeny; // max 100, azon tul egészségi allapota romik
+		int egeszsegiAllapot; // 0-10, ahol 0: elpusztult 
+		int optimalisSuruseg;
+
+		public NovenyFaj(string nev, int vizigeny, int egeszsegiAllapot, int optimalisSuruseg)
+		{
+			
+			this.nev = nev;
+			this.vizigeny = vizigeny;
+			this.egeszsegiAllapot = egeszsegiAllapot;
+			this.optimalisSuruseg = optimalisSuruseg;
+		}
+
+		public string Azonosito { get => this.nev.Substring(0,3); }
+		public string Nev { get => nev; set => nev = value; }
+		public int Vizigeny { get => vizigeny; set => vizigeny = value; }
+		public int EgeszsegiAllapot { get => egeszsegiAllapot; set => egeszsegiAllapot = value; }
+		public int OptimalisSuruseg { get => optimalisSuruseg; set => optimalisSuruseg = value; }
+
+
+		public override string ToString()
+		{
+			return $"{this.nev} ({this.Azonosito}) {this.vizigeny} {this.egeszsegiAllapot}";
+		}
 	}
 }
