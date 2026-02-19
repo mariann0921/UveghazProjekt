b7bd9cafcb6df733cf4de0726aa25a826c5ac455
user <user@petrik.hu>
2026-02-19 15:09:23 +0100

    Cella osztaly felkesz

--- Cella.cs
+++ Cella.cs
@@ -8,5 +8,55 @@ namespace UveghazProjekt
 {
 	internal class Cella
 	{
+		Pozicio poz;
+		NovenyFaj noveny;
+		int egyedSzam;
+		List<Riasztas> riasztasok;
+		List<Szenzor> szenzorok;
+		public Cella(Pozicio poz)
+		{
+			NovenyFaj noveny = null;
+			this.poz = poz;
+			egyedSzam = 0;
+			riasztasok = new List<Riasztas>();
+			szenzorok = new List<Szenzor>();
+		}
+
+		public int EgyedSzam { get => egyedSzam; set => egyedSzam = value; }
+		internal Pozicio Poz { get => poz; set => poz = value; }
+		internal NovenyFaj Noveny { get => noveny; set => noveny = value; }
+		internal List<Riasztas> Riasztasok { get => riasztasok; set => riasztasok = value; }
+		internal List<Szenzor> Szenzorok { get => szenzorok; set => szenzorok = value; }
+
+		public bool UresCella
+		{
+			get
+			{
+				return this.noveny == null;
+			}
+			
+		}
+
+		public bool Beultet(NovenyFaj noveny, int egyedSzam)
+		{
+			if (this.UresCella)
+			{
+				this.noveny = noveny;
+				this.egyedSzam = egyedSzam;
+				return true;
+			}
+			else if (noveny == this.noveny)
+			{
+				this.egyedSzam += egyedSzam;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+
+
 	}
 }
---
+++ Pozicio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UveghazProjekt
+{
+	internal class Pozicio
+	{
+		int x;
+		int y;
+
+		public Pozicio(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public int X { get => x; set => x = value; }
+		public int Y { get => y; set => y = value; }
+
+		public override string ToString()
+		{
+			return $"({this.x}, {this.y})";
+		}
+	}
+}
